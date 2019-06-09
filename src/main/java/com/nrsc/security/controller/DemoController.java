6d151fadc744e237f67a5219cd1ad0458b9e5b89
sunchuan <272420199@qq.com>
2019-06-09 23:14:48 +0800

    spring-security入门1---springboot项目搭建+简单修改Spring-Security的默认行为

---
+++ src/main/java/com/nrsc/security/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.nrsc.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello spring-security";
+    }
+}
