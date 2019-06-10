97aeb4ff8bfe58df2ad54ace097bb495f3dfb09a
sunchuan <272420199@qq.com>
2019-06-11 01:05:26 +0800

    spring-security入门2---自定义用户认证逻辑

---
+++ src/main/java/com/nrsc/security/config/MyUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.nrsc.security.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //点击页面的登陆时,你实际需要做的事为
+        //1.拿着用户名去数据库里查询其密码
+        //2.将拿到的用户名和密码封装到User里进行返回
+
+        log.info("用户名为:" + username);
+        //假设下面的密码是根据用户名获得的
+        String password = passwordEncoder.encode("123456");
+        log.error("password:" + password);
+        //return new User(username, password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        return new User(username, password, true,true,true,false,
+                AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}
--- src/main/java/com/nrsc/security/config/SecurityConfig.java
+++ src/main/java/com/nrsc/security/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package com.nrsc.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest()
                 .authenticated()
+                // .and()
+                // .httpBasic()
                 .and()
-                .httpBasic();
-               /* .and()
-                .formLogin();*/
+                .formLogin();
     }
 }
