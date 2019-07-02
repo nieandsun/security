9bd494560da2140180b1a9c65f7a6592ea06474c
sunchuan <272420199@qq.com>
2019-07-02 23:27:25 +0800

    spring-security入门8---获取用户对象信息

--- src/main/java/com/nrsc/security/server/controller/DemoController.java
+++ src/main/java/com/nrsc/security/server/controller/DemoController.java
@@ -1,5 +1,9 @@
 package com.nrsc.security.server.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +13,24 @@ public class DemoController {
     @GetMapping("/hello")
     public String sayHello() {
         return "hello spring-security";
+    }
+
+    @GetMapping("/me1")
+    public Object getCurrentUser1() {
+        //方式1，直接从SecurityContextHolder中拿到Authentication对象
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication;
+    }
+
+    @GetMapping("/me2")
+    public Object getCurrentUser2(Authentication authentication) {
+        //方式2---方式1的简写版
+        return authentication;
+    }
+
+    @GetMapping("/me3")
+    public Object getCurrentUser3(@AuthenticationPrincipal UserDetails user) {
+        //方式3，只获取User对象
+        return user;
     }
 }
