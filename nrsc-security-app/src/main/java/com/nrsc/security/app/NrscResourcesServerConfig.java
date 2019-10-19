aa73d2839d77e27a7a8f5fa1c503c6153c80bec2
sunchuan <272420199@qq.com>
2019-10-19 20:58:31 +0800

    【Spring Security OAuth开发APP认证框架】--- 将用户名密码登陆嫁接到Spring Security OAuth

--- nrsc-security-app/src/main/java/com/nrsc/security/app/NrscResourcesServerConfig.java
+++ nrsc-security-app/src/main/java/com/nrsc/security/app/NrscResourcesServerConfig.java
@@ -1,7 +1,18 @@
 package com.nrsc.security.app;
 
+import com.nrsc.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.nrsc.security.core.properties.NrscSecurityProperties;
+import com.nrsc.security.core.properties.SecurityConstants;
+import com.nrsc.security.core.social.SocialConfig;
+import com.nrsc.security.core.validate.code.ValidateCodeSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 /**
  * @author : Sun Chuan
@@ -10,5 +21,62 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class NrscResourcesServerConfig {
+public class NrscResourcesServerConfig  extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    protected AuthenticationSuccessHandler NRSCAuthenticationSuccessHandler;
+
+    @Autowired
+    protected AuthenticationFailureHandler NRSCAuthenticationFailureHandler;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    /**
+     * @see SocialConfig#nrscSocialSecurityConfig()
+     */
+    @Autowired
+    private SpringSocialConfigurer nrscSocialSecurityConfig;
+
+
+    @Autowired
+    private NrscSecurityProperties nrscSecurityProperties;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .successHandler(NRSCAuthenticationSuccessHandler)
+                .failureHandler(NRSCAuthenticationFailureHandler);
+
+        //验证码有一些问题：因为验证码会放到session里，校验也需要从session里取，
+        // 但是在token认证的情景下是不需session参与的
+        // 所以这里先注释掉，下篇文章再解决
+        http//.apply(validateCodeSecurityConfig)
+                //	.and()
+                .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                .apply(nrscSocialSecurityConfig)
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                        nrscSecurityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*",
+                        nrscSecurityProperties.getBrowser().getSignUpUrl(),
+                        nrscSecurityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+                        nrscSecurityProperties.getBrowser().getSignOutUrl(),
+                        "/user/regist")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable();
+    }
 }
--- nrsc-security-app/src/main/java/com/nrsc/security/app/authentication/NRSCAuthenticationSuccessHandler.java
+++ nrsc-security-app/src/main/java/com/nrsc/security/app/authentication/NRSCAuthenticationSuccessHandler.java
@@ -1,11 +1,19 @@
 package com.nrsc.security.app.authentication;
 
-import com.alibaba.fastjson.JSON;
-import com.nrsc.security.core.properties.LoginType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nrsc.security.core.properties.NrscSecurityProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -22,31 +30,102 @@ import java.io.IOException;
 @Component(value = "NRSCAuthenticationSuccessHandler")
 public class NRSCAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
-//    @Autowired
-//    private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
+    private SecurityProperties securityProperties;
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+    @Autowired
     private NrscSecurityProperties nrscSecurityProperties;
+
     /**
      * Authentication封装了用户认证成功的信息
      */
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
-                                        HttpServletResponse httpServletResponse,
-                                        Authentication authentication)
-            throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
 
-        log.info("登陆成功");
+        logger.info("登录成功");
+        //请求头的Authorization里存放了ClientId和ClientSecret
+        //从请求头里获取Authorization信息可参考BasicAuthenticationFilter类
+        String header = request.getHeader("Authorization");
 
-        //如果设置了登陆成功返回json,则按如下方式进行返回
-        if (LoginType.JSON.equals(nrscSecurityProperties.getBrowser().getLoginType())) {
-            //设置返回内容的数据形式和编码格式
-            httpServletResponse.setContentType("application/json;charset=UTF-8");
-            //将户认证成功的信息以json数据的形式返回给前端
-            httpServletResponse.getWriter().write(JSON.toJSONString(authentication));
-        } else {
-            //如果登陆成功后不想返回json,可以按照spring-security默认方式进行处理(登录成功后会跳转到引发登录的请求上)
-            super.onAuthenticationSuccess(httpServletRequest, httpServletResponse, authentication);
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
         }
+
+        String[] tokens = extractAndDecodeHeader(header, request);
+        assert tokens.length == 2;
+
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        // 根据clientId获取ClientDetails对象 --- ClientDetails为第三方应用的信息
+        // 现在配置在了yml文件里，真实项目中应该放在数据库里
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+        // 对获取到的clientDetails进行校验
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId对应的配置信息不存在:" + clientId);
+        } else if (!StringUtils.equals(clientDetails.getClientSecret(), clientSecret)) {
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配:" + clientId);
+        }
+
+        //第一个参数为请求参数的一个Map集合，
+        // 在Spring Security OAuth的源码里要用这个Map里的用户名+密码或授权码来生成Authentication对象,
+        // 但我们已经获取到了Authentication对象，所以这里可以直接传一个空的Map
+        //第三个参数为scope即请求的权限 ---》这里的策略是获得的ClientDetails对象里配了什么权限就给什么权限 //todo
+        //第四个参数为指定什么模式 比如密码模式为password，授权码模式为authorization_code，
+        // 这里我们写一个自定义模式custom
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "custom");
+
+        //获取OAuth2Request对象
+        //源码中是这么写的 --- todo 有兴趣的可以看一下
+        // OAuth2Request storedOAuth2Request = getRequestFactory().createOAuth2Request(client, tokenRequest);
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        //new出一个OAuth2Authentication对象
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        //生成token
+        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        //将生成的token返回
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(token));
+
+    }
+
+    /**
+     * 从header获取Authentication信息 --- 》 clientId和clientSecret
+     * @param header
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request) throws IOException {
+
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException e) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, "UTF-8");
+
+        int delim = token.indexOf(":");
+
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        }
+        return new String[]{token.substring(0, delim), token.substring(delim + 1)};
     }
 }
